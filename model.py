77d7f575ececadcf97ee662fe18be879c0d4f2de
Zijun Yao <393314766@qq.com>
2018-03-11 20:55:40 +0100

    Update and rename tpot.py to model.py

--- model.py
+++ model.py
@@ -212,7 +212,7 @@ with timer("Data preprocessing"):
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=20)
     
-pipeline_optimizer = TPOTClassifier(generations=5, population_size=20, cv=5,
+pipeline_optimizer = TPOTClassifier(generations=100, population_size=100, cv=5,
                                     random_state=42, verbosity=2)
 pipeline_optimizer.fit(X_train, y_train)
 print(pipeline_optimizer.score(X_test, y_test))
